affa761043c371c6eb42acca40dfc0dd7c7e3979
hamza jaffer <hamza-jaffar>
2026-07-25 00:31:19 +0500

    Add Role&Permission and Organization User Migration and Model

---
+++ app/Models/RolePermission.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable('role_id', 'permission_id')]
+class RolePermission extends Model
+{
+    //
+}
